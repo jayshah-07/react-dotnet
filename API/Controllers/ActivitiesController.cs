381e925e1e68a6e59b08e1eade0c7c7c65545b58
orderndine <61357570+orderndine@users.noreply.github.com>
2021-03-12 12:17:50 +0530

    first lesson

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController:BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context) {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivies(){
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id){
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
